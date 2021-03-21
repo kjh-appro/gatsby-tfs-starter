aab3e34938a56da52b5628f8be678c162e2c99ae
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-03-22 01:48:33 +0900

    Update BigAvatar.js

--- src/components/Avatar/BigAvatar.js
+++ src/components/Avatar/BigAvatar.js
@@ -2,7 +2,7 @@ import React from "react";
 import AvatarLinks from "./AvatarLinks";
 import { Link } from "gatsby";
 import siteConfig from "../../../data/SiteConfig";
-import logo from "../images/logo.png";
+import logo from "../../../data/logo.png";
 
 /** @jsx jsx */
 import { Styled, jsx } from "theme-ui";
